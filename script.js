6f436782359ca1be04b1734f726ea33965bd5554
Ayan Khan <khanayan00410@gmail.com>
2024-10-27 21:57:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text",{
+    strings: ["Web Developer","Frontend Developer","Web Designer"],
+    typeSpeed: 100,
+    backSpeed: 10,
+    backDelay: 1000,
+    loop: true
+});
